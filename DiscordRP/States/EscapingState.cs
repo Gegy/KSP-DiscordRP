9530cced7812634d86ec72e878ee7fc70971eefe
DeltaDizzy <EJBraswell@gmail.com>
2018-11-29 12:02:05 -0600

    maybe ?? will fix bugs?

--- DiscordRP/States/EscapingState.cs
+++ DiscordRP/States/EscapingState.cs
@@ -30,14 +30,14 @@ namespace DiscordRP.States
 
         public DiscordRpc.RichPresence create()
         {
-            string state = state = string.Format("Escaping {0}", body.displayName);
+            string state = state = string.Format("Escaping {0}", body.name ?? body.displayName);
 
             return new DiscordRpc.RichPresence()
             {
                 state = state,
                 details = "At escape velocity",
-                largeImageKey = string.Format("body_{0}", body.displayName.ToLower()),
-                largeImageText = body.displayName,
+                largeImageKey = string.Format("body_{0}", body.displayName.ToLower() ?? body.name.ToLower()),
+                largeImageText = body.displayName ?? body.name,
                 startTimestamp = startTimestamp,
                 smallImageKey = Utils.GetSmallFlightIcon(paused),
                 smallImageText = Utils.GetSmallFlightIconDetails(paused),
